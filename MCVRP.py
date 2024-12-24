583a82e0cbc97607b30c50d7293a153d408b0b06
JingyangZhao <113542122+JingyangZhao@users.noreply.github.com>
2024-12-24 21:29:13 +0800

    Update MCVRP.py

--- MCVRP.py
+++ MCVRP.py
@@ -12,7 +12,7 @@ def function(rho, tau, epsilon):
     theta = 1-tau
     return (1+zeta(rho,tau,epsilon))/theta + (1-tau-theta)/(theta*(1-tau)) + 3*epsilon/(1-theta) + 3*rho/(1-rho)/(1-tau) - 1
 ############################################################################################################################################
-def f(alpha, epsilon): # t,r<=1/6
+def f(epsilon): # t,r<=1/6
     min_found=1000
     for i in range(1667):
         for j in range(1667):
@@ -21,13 +21,11 @@ def f(alpha, epsilon): # t,r<=1/6
             if min_found > tem: min_found = tem
     return min_found
 ############################################################################################################################################
-alpha=1.5
-############################################################################################################################################
 #""" splittable
 if 1:
     epsilon=1/3000
-    g=f(alpha, epsilon) # f(epsilon) in the paper
-    print(3+2*f(alpha, epsilon)); print(4-2*epsilon)
+    g=f(epsilon) # f(epsilon) in the paper
+    print(3+2*f(epsilon)); print(4-2*epsilon)
     
     if 4-2*epsilon < 3+2*g: print("epsilon should be smaller")
     else: print("epsilon can be bigger")
@@ -35,7 +33,7 @@ if 1:
 ### unsplittable
 elif 0:
     epsilon=1/100000
-    g=f(alpha, epsilon) # f(epsilon) in the paper
+    g=f(epsilon) # f(epsilon) in the paper
     print(3+math.log(2)+2*g); print(4-2*epsilon)
     
     if 4-2*epsilon < 3+math.log(2)+2*g: print("epsilon should be smaller")
@@ -44,7 +42,7 @@ elif 0:
 ### splittable with fixed capacity
 elif 0:
     epsilon=1/9000
-    g=f(alpha, epsilon) # f(epsilon) in the paper
+    g=f(epsilon) # f(epsilon) in the paper
     print(3+2*g); print(3+math.log(2)-epsilon)
     
     if 3+math.log(2)+math.log(1-epsilon) < 3+2*g: print("epsilon should be smaller")
